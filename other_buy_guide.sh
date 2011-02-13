a4ebe5585965047ad0b6f8b7a47744afe77bc724
Joe Balough <scallopedllama@gmail.com>
2011-02-13 18:55:20 +0900

    got the newegg search functionality working fully. added kakaku url to the kakaku.tsv file

--- other_buy_guide.sh
+++ other_buy_guide.sh
@@ -90,13 +90,13 @@ done
 # If you've never used sed before, it's probably a good idea to go look at a tutorial but here is the jist of what we're doing.
 # Get an example line that contains ONE listing from the results page.
 # Kakaku.com example line: "...<td class="item">Corsair<p><a href="http://kakaku.com/item/K0000045190/"><strong>CMPSU-850HX</strong></a></p></td><td class="td-price"><a href="http://kakaku.com/item/K0000045190/">&#165;15,999</a><br><span>アクロス</span></td><td>25店舗</td><td>22位</td><td>31位</td><td class="select">4.74<br><a href="http://review.kakaku.com/review/K0000045190/">(17件)</a></td><td><a href="http://bbs.kakaku.com/bbs/K0000045190/">131件</a></td><td>-</td><td>09/07/07</td><td>850W&nbsp;</td><td>ATX/EPS&nbsp;</td><td>150x180x86mm&nbsp;</td><td class="end">&yen;18.82&nbsp;</td></tr>"
-# 1: manufacturer, 2: model, 3: price, 4: price pt 2, 5: score, 6: # reviews, 7: Power rating, 8: size
+# 1: manufacturer, 2: url, 3: model, 4: price, 5: price pt 2, 6: score, 7: # reviews, 8: Power rating, 9: size
 for (( i=1; i<=$1; i++ ))
 do
   j=$(printf "%02d" "$i")
   # Append to the tsv file
-  # Kak    "...<td class="item">Corsair <p><a href=..><strong>CM..HX</strong></a></p></td><td class=\"td-price\"><a href=..>&#165; Price1   , price2    </a><br><span>..</span></td><td>..</td><td>..</td><td>..</td><td class=\"select\"> score<br><a href=..>( #reviews 件)</a></td><td><a href=..>..</a></td><td>..</td><td>..</td><td>  PWR &nbsp;</td><td> SIZE &nbsp;</td><td>..</td><td class="end">..</td></tr>"
-  sed -n "s:^.*<td class=\"item\">\(.*\)<p><a href=.*><strong>\(.*\)</strong></a></p></td><td class=\"td-price\"><a href=.*>&#165;\([0-9]*\),*\([0-9]*\)</a><br><span>.*</span></td><td>.*</td><td>.*</td><td>.*</td><td class=\"select\">\(.*\)<br><a href=.*>(\([0-9]*\)件)</a></td><td><a href=.*>.*</a></td><td>.*</td><td>.*</td><td>\(.*\)&nbsp;</td><td>\(.*\)&nbsp;</td><td>.*</td><td class=\"end\">.*</td></tr>:\1\t\2\t\3\4\t\5\t\6\t\7\t\8:gp" search.p$j.html >> kakaku.tsv
+  # Kak    "...<td class="item">Corsair <p><a href="   URL  "><strong>CM..HX</strong></a></p></td><td class=\"td-price\"><a href=..>&#165; Price1   , price2    </a><br><span>..</span></td><td>..</td><td>..</td><td>..</td><td class=\"select\"> score<br><a href=..>( #reviews 件)</a></td><td><a href=..>..</a></td><td>..</td><td>..</td><td>  PWR &nbsp;</td><td> SIZE &nbsp;</td><td>..</td><td class="end">..</td></tr>"
+  sed -n "s:^.*<td class=\"item\">\(.*\)<p><a href=\"\(.*\)\"><strong>\(.*\)</strong></a></p></td><td class=\"td-price\"><a href=.*>&#165;\([0-9]*\),*\([0-9]*\)</a><br><span>.*</span></td><td>.*</td><td>.*</td><td>.*</td><td class=\"select\">\(.*\)<br><a href=.*>(\([0-9]*\)件)</a></td><td><a href=.*>.*</a></td><td>.*</td><td>.*</td><td>\(.*\)&nbsp;</td><td>\(.*\)&nbsp;</td><td>.*</td><td class=\"end\">.*</td></tr>:\1\t\3\t\4\5\t\6\t\7\t\8\t\9\t\2:gp" search.p$j.html >> kakaku.tsv
 done
 
 # make sure it has a reasonable number of entries
@@ -111,6 +111,51 @@ else
   echo "Got $search_entries search results."
 fi
 
+# Loop through checking each result
+for (( i=1; i<=$search_entries; i++ ))
+do
+  part_no_unmodified=$(sed -n "$i p" kakaku.tsv| awk -v FS="\t" '{print($2)}')
+  # The part number we want to search for on newegg is the LAST word in the string that contains a '-' character.
+  # Some parts have somthing like INFINITI-JC in the first word and EIN720AWT-JC in the second.
+  # First we get the last word with a - in it, Account for the possibility that the first word had the -, then remove preceeding words if the last one is 4 or more characters, remove any /whatever from the end, and any (whatever) from the end
+  part_no=$(echo "$part_no_unmodified" | sed "s:.* \([^ ]*-[^ ]*\) *[^-]*$:\1:" | sed "s:^\([^ ]*-[^ ]*\) .*$:\1:" | sed "s:^[^-]* \([^ ][^ ][^ ][^ W][^ ]*\)$:\1:" | sed "s:^\(.*\)/.*$:\1:" | sed "s:^\(.*\)(.*)$:\1:")
+
+  echo -n "  Searching for $part_no on Newegg.com: "
+
+  # Newegg search url: http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=$part_no
+  # This can either return a product page, a search with no results, or a search with more than one result
+  newegg_search_url="http://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=$part_no"
+  wget -q -O newegg_search.html $newegg_search_url
+
+  if [ $(grep -c "<h2>Search Tips</h2>" newegg_search.html) -gt 0 ]
+  then
+    echo "no results."
+    continue
+  fi
+
+  rating=""
+  newegg_url=""
+  no_reviews=""
+  if [ $(grep -c "<dd>Item#" newegg_search.html) -gt 0 ]
+  then
+    rating=$(sed -n "s:.*<img class=\"eggs r[0-9] screen\" title=\"\([0-9]\) out of 5 eggs\".*:\1:p" newegg_search.html)
+    newegg_url="$newegg_search_url"
+    no_reviews=$(sed -n "s:.*\"display\:none;\">[0-9]/5</span>(\([0-9]*\)&nbsp;reviews)</a>.*:\1:p" newegg_search.html)
+  else
+    # This sed command gets the top result and gets its rating and the url of the review for that product.
+    rating=$(grep -m 1 "<a title=\"Rating +" newegg_search.html | sed -n "s:.*<a title=\"Rating + \([0-9]\)\" href=\".*\" class=\"itemRating\"><span class=\"eggs r[0-9]\">&nbsp;</span>.*:\1:p")
+    newegg_url=$(grep -m 1 "<a title=\"Rating +" newegg_search.html | sed -n "s:.*<a title=\"Rating + [0-9]\" href=\"\(.*\)\" class=\"itemRating\"><span class=\"eggs r[0-9]\">&nbsp;</span>.*:\1:p")
+    no_reviews=$(grep -m 1 "class=\"itemRating\">" newegg_search.html | sed -n "s:.*class=\"itemRating\"><span class=\"eggs r[0-9]\">&nbsp;</span> (\([0-9]*\))</a>.*:\1:p")
+  fi
+  echo "found with rating of $rating eggs after $no_reviews reviews."
+  echo -e "$part_no_unmodified\t$rating\t$no_reviews\t$newegg_url" >> newegg_results.tsv
+
+  rm newegg_search.html
+
+done
+
+rm kakaku.tsv
+rm search.p??.html
 exit 0
 
 
